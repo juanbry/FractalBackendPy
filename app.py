e03c04be17c42db15adcfcb5dfb06437f834fba3
Juan <marcoreus24102001@gmail.com>
2025-05-08 18:16:32 -0500

    Ultimo App.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hola Fractal'
+    return 'Hola Fractal, esto fue hecho en Flask por Juan Carranza'
 
 if __name__ == '__main__':
     app.run(debug=True)  # Esto ejecutará el servidor de desarrollo
